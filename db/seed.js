b0271351035b285104d8168a2fabb6545673573c
Jordan Hawley <mjthriller1993@gmail.com>
2021-05-08 10:29:38 -0700

    part 2 finished

--- db/seed.js
+++ db/seed.js
@@ -1,121 +1,135 @@
-const { client, getAllUsers, createUser, updateUser } = require("./index");
-
-async function dropTables() {
-  try {
-    console.log("Starting to drop tables...");
-
-    await client.query(`
+const {  
+    client,
+    createUser,
+    updateUser,
+    getAllUsers,
+    getUserById,
+    createPost,
+    updatePost,
+    getAllPosts,
+    getPostsByUser
+  } = require('./index');
+  
+  async function dropTables() {
+    try {
+      console.log("Starting to drop tables...");
+  
+      // have to make sure to drop in correct order
+      await client.query(`
+        DROP TABLE IF EXISTS posts;
         DROP TABLE IF EXISTS users;
       `);
-
-    console.log("Finished dropping tables!");
-  } catch (error) {
-    console.error("Error dropping tables!");
-    throw error;
+  
+      console.log("Finished dropping tables!");
+    } catch (error) {
+      console.error("Error dropping tables!");
+      throw error;
+    }
   }
-}
-
-async function createTables() {
-  try {
-    console.log("Starting to build tables...");
-
-    await client.query(`
+  
+  async function createTables() {
+    try {
+      console.log("Starting to build tables...");
+  
+      await client.query(`
         CREATE TABLE users (
           id SERIAL PRIMARY KEY,
           username varchar(255) UNIQUE NOT NULL,
           password varchar(255) NOT NULL,
-          name VARCHAR(255) NOT NULL,
-          location VARCHAR(255) NOT NULL,
-          active BOOLEAN DEFAULT true
+          name varchar(255) NOT NULL,
+          location varchar(255) NOT NULL,
+          active boolean DEFAULT true
         );
         CREATE TABLE posts (
-            id SERIAL PRIMARY KEY,
-            "authorId" INTEGER REFERENCES users(id) NOT NULL,
-            title VARCHAR(255) NOT NULL,
-            content TEXT NOT NULL,
-            active BOOLEAN DEFAULT true
+          id SERIAL PRIMARY KEY,
+          "authorId" INTEGER REFERENCES users(id),
+          title varchar(255) NOT NULL,
+          content TEXT NOT NULL,
+          active BOOLEAN DEFAULT true
         );
       `);
-
-    console.log("Finished building tables!");
-  } catch (error) {
-    console.error("Error building tables!");
-    throw error;
+  
+      console.log("Finished building tables!");
+    } catch (error) {
+      console.error("Error building tables!");
+      throw error;
+    }
   }
-}
-
-async function createInitialUsers() {
-  try {
-    console.log("Starting to create users...");
-
-    await createUser({
-      username: "albert",
-      password: "bertie99",
-      name: "Al Bert",
-      location: "Sidney, Australia",
-    });
-    await createUser({
-      username: "sandra",
-      password: "2sandy4me",
-      name: "Just Sandra",
-      location: "Ain't tellin'",
-    });
-    await createUser({
-      username: "glamgal",
-      password: "soglam",
-      name: "Joshua",
-      location: "Upper East Side",
-    });
-
-    console.log("Finished creating users!");
-  } catch (error) {
-    console.error("Error creating users!");
-    throw error;
+  
+  async function createInitialUsers() {
+    try {
+      console.log("Starting to create users...");
+  
+      await createUser({ 
+        username: 'albert', 
+        password: 'bertie99',
+        name: 'Al Bert',
+        location: 'Sidney, Australia' 
+      });
+      await createUser({ 
+        username: 'sandra', 
+        password: '2sandy4me',
+        name: 'Just Sandra',
+        location: 'Ain\'t tellin\''
+      });
+      await createUser({ 
+        username: 'glamgal',
+        password: 'soglam',
+        name: 'Joshua',
+        location: 'Upper East Side'
+      });
+  
+      console.log("Finished creating users!");
+    } catch (error) {
+      console.error("Error creating users!");
+      throw error;
+    }
   }
-}
-
-async function createInitialPosts() {
+  
+  async function createInitialPosts() {
     try {
       const [albert, sandra, glamgal] = await getAllUsers();
   
+      console.log("Starting to create posts...");
       await createPost({
         authorId: albert.id,
         title: "First Post",
         content: "This is my first post. I hope I love writing blogs as much as I love writing them."
       });
-
+  
       await createPost({
         authorId: sandra.id,
-        title: "First Post",
-        content: "This is my first post. I hope I love writing blogs as much as I love writing them."
-      });
-
-      await createPost({
-        authorId: glamgal.id,
-        title: "First Post",
-        content: "This is my first post. I hope I love writing blogs as much as I love writing them."
+        title: "How does this work?",
+        content: "Seriously, does this even do anything?"
       });
   
-      // a couple more
+      await createPost({
+        authorId: glamgal.id,
+        title: "Living the Glam Life",
+        content: "Do you even? I swear that half of you are posing."
+      });
+      console.log("Finished creating posts!");
     } catch (error) {
+      console.log("Error creating posts!");
       throw error;
     }
   }
-
-async function rebuildDB() {
-  try {
-    client.connect();
-
-    await dropTables();
-    await createTables();
-    await createInitialUsers();
-    await createInitialPosts();
-  } catch (error) {
-    throw error;
+  
+  async function rebuildDB() {
+    try {
+      client.connect();
+  
+      await dropTables();
+      await createTables();
+      await createInitialUsers();
+      await createInitialPosts();
+    } catch (error) {
+      console.log("Error during rebuildDB")
+      throw error;
+    }
   }
-}
-
-async function testDB() {
+  
+  async function testDB() {
     try {
       console.log("Starting to test database...");
   
@@ -151,8 +165,9 @@ async function testDB() {
       throw error;
     }
   }
-
-rebuildDB()
-  .then(testDB)
-  .catch(console.error)
-  .finally(() => client.end());
+  
+  
+  rebuildDB()
+    .then(testDB)
+    .catch(console.error)
+    .finally(() => client.end());
