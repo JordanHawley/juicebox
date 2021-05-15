c2e6820de29e5bfa5518c41575703a4446eb3c72
Jordan Hawley <mjthriller1993@gmail.com>
2021-05-15 14:23:53 -0700

    day 3 has been finished

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,35 @@
-const express = require('express');
+const express = require("express");
 const postsRouter = express.Router();
-const { getAllPosts } = require('../db');
+const { getAllPosts, createPost, updatePost, getPostById, getPostsByTagName } = require("../db");
+const tagsRouter = require("./tags");
+const { requireUser } = require("./utils");
+
+postsRouter.post("/", requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    postData.authorId = requireUser.authorId;
+    postData.title = title;
+    postData.content = content;
+
+    const post = await createPost(postData);
+
+    if(postData) {
+    res.send({ post });
+    }
+
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
@@ -8,12 +37,101 @@ postsRouter.use((req, res, next) => {
   next();
 });
 
+postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
+  const { postId } = req.params;
+  const { title, content, tags } = req.body;
+
+  const updateFields = {};
+
+  if (tags && tags.length > 0) {
+    updateFields.tags = tags.trim().split(/\s+/);
+  }
+
+  if (title) {
+    updateFields.title = title;
+  }
+
+  if (content) {
+    updateFields.content = content;
+  }
+
+  try {
+    const originalPost = await getPostById(postId);
+
+    if (originalPost.author.id === req.user.id) {
+      const updatedPost = await updatePost(postId, updateFields);
+      res.send({ post: updatedPost })
+    } else {
+      next({
+        name: 'UnauthorizedUserError',
+        message: 'You cannot update a post that is not yours'
+      })
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+postsRouter.delete('/:postId', requireUser, async (req, res, next) => {
+  try {
+    const post = await getPostById(req.params.postId);
+
+    if (post && post.author.id === req.user.id) {
+      const updatedPost = await updatePost(post.id, { active: false });
+
+      res.send({ post: updatedPost });
+    } else {
+      // if there was a post, throw UnauthorizedUserError, otherwise throw PostNotFoundError
+      next(post ? { 
+        name: "UnauthorizedUserError",
+        message: "You cannot delete a post which is not yours"
+      } : {
+        name: "PostNotFoundError",
+        message: "That post does not exist"
+      });
+    }
+
+  } catch ({ name, message }) {
+    next({ name, message })
+  }
+});
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+  // read the tagname from the params
+  const { tagName } = req.params
+  try {
+    const postByTag = await getPostsByTagName(tagName)
+    res.send({ post:postByTag })
+
+  } catch ({ name, message }) {
+    next({ name, message })
+  }
+});
+
 postsRouter.get('/', async (req, res) => {
-    const posts = await getAllPosts();
-  
+  try {
+    const allPosts = await getAllPosts();
+
+    const posts = allPosts.filter(post => {
+      if (post.active) {
+        return true;
+      }
+    
+      // the post is not active, but it belogs to the current user
+      if (req.user && post.author.id === req.user.id) {
+        return true;
+      }
+    
+      // none of the above are true
+      return false;
+    });
+
     res.send({
       posts
     });
-  });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
-module.exports = postsRouter;
+module.exports = postsRouter, tagsRouter;
--- db/index.js
+++ db/index.js
@@ -276,6 +276,15 @@ async function createTags(tagList) {
         WHERE id=$1;
       `, [postId]);
   
+      // THIS IS NEW
+      if (!post) {
+        throw {
+          name: "PostNotFoundError",
+          message: "Could not find a post with that postId"
+        };
+      }
+      // NEWNESS ENDS HERE
+  
       const { rows: tags } = await client.query(`
         SELECT tags.*
         FROM tags
